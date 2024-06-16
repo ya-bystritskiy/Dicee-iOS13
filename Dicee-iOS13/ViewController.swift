48d4245b50d607550329a22a044b04bd31d55c3f
ya-bystritskiy <bystritskii.yaroslav@gmail.com>
2024-06-16 18:49:19 +0300

    change image

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DiceImageView1: UIImageView!
+    @IBOutlet weak var DiceImageView2: UIImageView!
     override func viewDidLoad() {
+        
+        DiceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        DiceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func RollButttonPressed(_ sender: Any) {
+        
+        DiceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        DiceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
+
 
